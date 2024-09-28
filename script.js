a7dbeb3a90d4fae7e48465f594143dfb6ad7530d
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-28 17:28:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+    event.preventDefault()
+	let form = document.getElementById("form1")
+	let str = ""
+		for(let item of form.elements){
+		if(item.type !== "submit"){
+			str += `${item.value} ` 
+		}
+	}
+alert(str.trim())
 }
